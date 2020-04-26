ad69bccf6389c0e318cee9adb2c0f9cc1faa74ad
uggletoggle <42126135+uggletoggle@users.noreply.github.com>
2020-04-26 18:48:29 -0300

    server

--- src/server.js
+++ src/server.js
@@ -5,4 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ng-blog'));
 app.get('/', function(req, res) {
-            res.sendFile(path.join(__dirname + '/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ng-blog/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
