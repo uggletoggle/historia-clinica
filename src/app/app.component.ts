66a750f3547c048586b64d8a7443f70cb831633d
uggletoggle <42126135+uggletoggle@users.noreply.github.com>
2020-04-26 23:59:08 -0300

    dinamismo decrease

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
   
   mostrarSuperior = "none";
   mostrarMedia = "none";
+  mostrarCabecera = true;
 
   cabeza = false;
   abdomen = false;
@@ -27,19 +28,20 @@ export class AppComponent {
   leftpx:string =this.left+"px";
   toppx:string = this.top+"px";
 
-
+  ejecutando = false;
+  
 
   @HostListener('mousemove', ['$event'])
   actualizarPosicion(event: MouseEvent){
     if (this.width>=1800 && event.target["id"] == "img1"){
-      if ( event.clientY <550 && event.clientY >350){
+      if ( event.clientY <=550 && event.clientY >=350){
         this.abdomen = true;
         this.cabeza = false;
         this.mostrarSuperior="none"
         this.mostrarMedia="block"
         this.top = -940;
         this.toppx = this.top+"px"
-      } else if (event.clientY < 350){
+      } else if (event.clientY <= 350){
         this.top = -290;
         this.toppx = this.top+"px"
         this.mostrarSuperior="block"
@@ -47,41 +49,64 @@ export class AppComponent {
         this.abdomen = false;
         this.cabeza = true;
       }
+    } else if (this.width>=800 && event.target["id"] != "img1"){
+      this.decrease();
+      this.ejecutando = true;
     }
   }
 
-  @HostListener('mouseenter', ['$event'])
-   async actualizarTop(event: MouseEvent) {
-     await this.delay(1);
-    if (this.width < 1800){
-    // console.log(event.target["id"]);
-    // console.log(event.clientY)
-    if (event.target["id"] == "img1" && event.clientY <550 && event.clientY >350){
-      this.top = event.clientY + this.fixedTop;
-      this.toppx = this.top+"px"
+//   @HostListener('mouseenter', ['$event'])
+//    async actualizarTop(event: MouseEvent) {
+//      await this.delay(1);
+//      console.log(event)
+//     if (this.width < 1800){
+//     // console.log(event.target["id"]);
+//     // console.log(event.clientY)
+//     if (event.target["id"] == "img1" && event.clientY <550 && event.clientY >350){
+//       this.top = event.clientY + this.fixedTop;
+//       this.toppx = this.top+"px"
       
-    } 
-  } 
-    else{
-      if (event.target["id"] == "img1" && event.clientY <550 && event.clientY >350){
-        this.abdomen = true;
-        this.cabeza = false;
-        this.mostrarSuperior="none"
-        this.mostrarMedia="block"
-      } else if (event.target["id"] == "img1" && event.clientY <350){
-        this.mostrarSuperior="block"
-        this.mostrarMedia="none"
-        this.abdomen = false;
-        this.cabeza = true;
-      }
-    }
- }
+//     } 
+//   } 
+//     else{
+//       if (event.target["id"] == "img1" && event.clientY <550 && event.clientY >350){
+//         this.abdomen = true;
+//         this.cabeza = false;
+//         this.mostrarSuperior="none"
+//         this.mostrarMedia="block"
+//       } else if (event.target["id"] == "img1" && event.clientY <350){
+//         this.mostrarSuperior="block"
+//         this.mostrarMedia="none"
+//         this.abdomen = false;
+//         this.cabeza = true;
+//       }
+//     }
+//  }
 
  
+async decrease(){ 
+  await this.delay(100);
+  if (this.width >= 800 && this.ejecutando == true) {
+    this.listo=false;
+    this.left += 25
+    
+    this.leftpx = this.left+"px";
+    this.width = this.width - this.difference;
+    this.decrease();
+  } else{
+    this.ejecutando=false;
+    this.mostrarCabecera = true;
+    this.listo=true;
+    this.top = -290;
+    this.toppx = this.top+"px"
+  }   
+}
+
+  
 
   async increase(){     
-    await this.delay(1);
-    if (this.width < 1800) {
+    await this.delay(10);
+    if (this.width <= 1800) {
     
       this.left -= 25
       
@@ -89,11 +114,9 @@ export class AppComponent {
       this.width = this.width + this.difference;
       this.increase();
     } else{
-      
+      this.mostrarCabecera = false;
       this.listo=true;
-    }
-
-    
+    }   
   }
 
 
